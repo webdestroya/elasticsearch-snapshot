2ff90ff1ed4de39efdbe9cb644093561835fd9cf
Mitch Dempsey <mrdempsey@gmail.com>
2015-07-03 22:44:47 -0700

    updates

--- README.md
+++ README.md
@@ -4,6 +4,27 @@ This container can be used to easily manage Elasticsearch snapshots stored on an
 
 This assumes you have installed and properly configured the Elasticsearch [cloud-aws](https://github.com/elastic/elasticsearch-cloud-aws) plugin.
 
+## Environment Variables
+
+* `ELASTICSEARCH_URL` *(default: `http://elasticsearch-9200:9200`)*
+  * The URL to reach your Elasticsearch server
+* `ESS_CREATE_IF_MISSING` *(defaut: false)*
+  * If this is `true` and the snapshot repository does not exist, then attempt to create it.
+* `ESS_MAX_SNAPSHOTS` *(defaut: 100)*
+  * The maximum number of snapshots to allow. Set to `0` to disable entirely.
+  * Note: Only snapshots with a `scheduled-` prefix will be counted and deleted. All other snapshots will not count against this limit.
+* `ESS_REPO_NAME` **required**
+  * The name of the repository to create snapshots under.
+* `ESS_WAIT_FOR_COMPLETION` *(default: true)*
+  * Whether the execution should wait until the snapshot has been created before exiting.
+
+If `ESS_CREATE_IF_MISSING` is set to `true` then the following are **required**:
+* `ESS_REPO_SETTINGS`
+  * A json payload that represents the `settings` part of a repository creation.
+* `ESS_REPO_TYPE`
+  * The repository type field. Common ones include `fs` and `s3`.
+
+For more details about repository creation, see the [Elasticsearch documentation](https://www.elastic.co/guide/en/elasticsearch/reference/1.6/modules-snapshots.html#_repositories)
 
 ## Usage
 This is best used with a timer and service combination in your cluster.
@@ -28,5 +49,28 @@ MachineOf=%p.service
 
 ```
 # elasticsearch-snapshot.service
+[Unit]
+Description=Elasticsearch Scheduled Snapshot
+Requires=docker.service
+After=docker.service
+
+[Service]
+TimeoutStartSec=0
+Type=simple
+
+ExecStartPre=-/usr/bin/docker pull webdestroya/elasticsearch-snapshot
+
+ExecStart=/usr/bin/docker run --rm=true \
+  -e ELASTICSEARCH_URL=http://elasticsearch-9200:9200 \
+  -e ESS_REPO_NAME=s3_repository \
+  -e ESS_MAX_SNAPSHOTS=100 \
+  -e ESS_WAIT_FOR_COMPLETION=true \
+  webdestroya/elasticsearch-snapshot
+
+[Install]
+WantedBy=multi-user.target
+
+[X-Fleet]
+Conflicts=%p.service
 
 ```
--- snapshot.sh
+++ snapshot.sh
@@ -4,7 +4,7 @@ ELASTICSEARCH_URL=${ELASTICSEARCH_URL-"http://elasticsearch-9200:9200"}
 repo_name=${ESS_REPO_NAME?"You must include a repository name"}
 create_if_missing=${ESS_CREATE_IF_MISSING-false}
 max_snapshots=${ESS_MAX_SNAPSHOTS-100}
-wait_for_completion=${ESS_WAIT_FOR_COMPLETION-false}
+wait_for_completion=${ESS_WAIT_FOR_COMPLETION-true}
 
 snapshot_timestamp=$(date -u +%s)
 
@@ -20,53 +20,66 @@ if [[ $repository_exists == "false" ]]; then
 
     create_result=$(curl -s -XPUT $ELASTICSEARCH_URL/_snapshot/$repo_name -d "{\"type\":\"$repo_type\",\"settings\":$repo_settings}")
 
-    result=$(echo $create_result | jq --raw-output "has(\"acknowledged\")")
+    result=$(echo $create_result | jq --raw-output .acknowledged)
     if [[ $result == "true" ]]; then
       echo "Repository '$repo_name' created!"
     else
-      echo "Unable to create repository '$repo_name'"
+      echo "ERROR: Unable to create repository '$repo_name'"
       echo $create_result
       exit 1
     fi
   else
-    echo "Repository '$repo_name' does not exist!"
+    echo "ERROR: Repository '$repo_name' does not exist!"
     exit 1;
   fi
 fi
 
 # Check for existing number of snapshots
-snapshot_list=$(curl -s -o /tmp/ess_snapshot_list.json $ELASTICSEARCH_URL/_snapshot/$repo_name/_all)
+snapshot_list=$(curl -s $ELASTICSEARCH_URL/_snapshot/$repo_name/_all)
 
 # Does this snapshot already exist??
 already_exists_count=$(echo $snapshot_list | jq --raw-output ".snapshots[].snapshot | contains(\"scheduled-$snapshot_timestamp\")" | grep -c true)
 if [[ $already_exists_count -gt 0 ]]; then
-  echo "The snapshot 'scheduled-$snapshot_timestamp' already exists in the repository '$repo_name'. Please wait 1 second."
+  echo "ERROR: The snapshot 'scheduled-$snapshot_timestamp' already exists in the repository '$repo_name'. Please wait 1 second."
+  exit 1
 fi
 
+# We only bother with deletions if the max_snapshots is greater than 0
+if [[ $max_snapshots -gt 0 ]]; then
 
-num_snapshots=$(echo $snapshot_list | jq --raw-output ".snapshots[].snapshot" | grep -E ^scheduled- | wc -l)
+  # How many of OUR snapshots already exist
+  num_snapshots=$(echo $snapshot_list | jq --raw-output ".snapshots[].snapshot" | grep -E ^scheduled- | wc -l)
 
-# Check if we need to delete any
-if [[ $num_snapshots -ge $max_snapshots ]]; then
-  num_snaps_to_remove=$(expr $num_snapshots - $max_snapshots)
-  echo "Found $num_snapshots existing snapshots. Maximum is $max_snapshots. Deleting extras."
+  # Check if we need to delete any
+  if [[ $num_snapshots -ge $max_snapshots ]]; then
+    num_snaps_to_remove=$(expr $num_snapshots - $max_snapshots)
+    echo "Found $num_snapshots existing snapshots. Maximum is $max_snapshots. Deleting $num_snaps_to_remove older snapshots"
 
-  for snapshot in $(echo $snapshot_list | jq --raw-output ".snapshots[].snapshot" | grep -E ^scheduled- | head -n $num_snaps_to_remove ); do
-    echo "Deleting snapshot $snapshot"
+    for snapshot in $(echo $snapshot_list | jq --raw-output ".snapshots[].snapshot" | grep -E ^scheduled- | head -n $num_snaps_to_remove ); do
+      echo "Deleting snapshot $snapshot"
 
-    curl -s -XDELETE $ELASTICSEARCH_URL/_snapshot/$repo_name/$snapshot
-  done
+      curl -s -XDELETE $ELASTICSEARCH_URL/_snapshot/$repo_name/$snapshot
+    done
+  fi
+
 fi
 
 
 # No deletions needed, so take a snapshot!
 result=$(curl -s -XPUT "$ELASTICSEARCH_URL/_snapshot/$repo_name/scheduled-${snapshot_timestamp}?wait_for_completion=${wait_for_completion}")
 
-if [[ "$(echo $result | jq .accepted)" == "true" ]]; then
-  echo "Snapshot 'scheduled-$snapshot_timestamp' in '$repo_name' created"
-  exit 0;
+if [[ $wait_for_completion == "true" ]]; then
+  if [[ "$(echo $result | jq --raw-output .snapshot.state)" == "SUCCESS" ]]; then
+    echo "Snapshot 'scheduled-$snapshot_timestamp' in '$repo_name' created"
+    exit 0;
+  fi
 else
-  echo "Failed to create snapshot 'scheduled-$snapshot_timestamp' in '$repo_name'"
-  echo $result
-  exit 1;
+  if [[ "$(echo $result | jq --raw-output .accepted)" == "true" ]]; then
+    echo "Snapshot 'scheduled-$snapshot_timestamp' in '$repo_name' created"
+    exit 0;
+  fi
 fi
+
+echo "ERROR: Failed to create snapshot 'scheduled-$snapshot_timestamp' in '$repo_name'"
+echo $result
+exit 1;
